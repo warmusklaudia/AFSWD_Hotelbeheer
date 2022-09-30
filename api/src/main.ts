e84432e5683a1c1c5de6da2e8f3d589517203610
Carmino <carmino.deschuijmree@student.howest.be>
2022-09-30 17:11:27 +0200

    feat: Added nestjs api

---
+++ api/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  const port: number = +process.env.PORT || 3003
+
+  await app.listen(3003);
+  console.info(`\nApplication is running on: \n http://localhost:${port}/graphql`);
+}
+bootstrap();
