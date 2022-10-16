aa3506527725cef1ebdd95c545bcfd44a80cd03c
Carmino <carmino.deschuijmere@student.howest.be>
2022-10-16 15:45:29 +0200

    feat: added auth routes

--- pwa/src/bootstrap/router.ts
+++ pwa/src/bootstrap/router.ts
@@ -1,13 +1,18 @@
 import {
   createRouter,
   createWebHistory,
+  RouteLocationNormalized,
   Router,
   RouteRecordRaw,
 } from 'vue-router'
+import useAuthentication from '../composables/useAuthentication'
+
+const { user } = useAuthentication()
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
+    //@ts-ignore
     component: () => import('../components/holders/AppHolder.vue'),
     children: [
       {
@@ -23,17 +28,17 @@ const routes: RouteRecordRaw[] = [
       {
         path: 'services',
         component: () => import('../screens/services/index.vue'),
-        meta: { needsAuthentication: false },
+        meta: { needsAuthentication: true },
       },
       {
         path: 'reservations',
         component: () => import('../screens/reservations/index.vue'),
-        meta: { needsAuthentication: false },
+        meta: { needsAuthentication: true },
       },
       {
         path: 'profile',
         component: () => import('../screens/Profile.vue'),
-        meta: { needsAuthentication: false },
+        meta: { needsAuthentication: true },
       },
     ],
   },
@@ -42,11 +47,40 @@ const routes: RouteRecordRaw[] = [
     name: 'ClientError',
     component: () => import('../screens/generic/ClientError.vue'),
   },
+  {
+    path: '/auth',
+    redirect: '/auth/login',
+    component: () => import('../components/holders/AuthHolder.vue'),
+    children: [
+      {
+        path: 'login',
+        component: () => import('../components/auth/Login.vue'),
+        meta: { cantAuthentication: true },
+      },
+      {
+        path: 'register',
+        component: () => import('../components/auth/Register.vue'),
+        meta: { cantAuthentication: true },
+      },
+      {
+        path: 'forgot-password',
+        component: () => import('../components/auth/ForgotPassword.vue'),
+      },
+    ],
+  },
 ]
 
 const router: Router = createRouter({
   history: createWebHistory(),
   routes,
 })
+
+router.beforeEach(
+  (to: RouteLocationNormalized, from: RouteLocationNormalized) => {
+    if (to.meta.needsAuthentication && !user.value) return 'auth/login'
+
+    if (to.meta.cantAuthentication && user.value) return '/'
+  },
+)
 
 export default router
