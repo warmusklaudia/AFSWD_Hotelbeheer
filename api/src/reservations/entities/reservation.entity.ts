44d83d6453351ae2c9dd9bfc2ff46186915f0f4c
Carmino <carmino.deschuijmere@student.howest.be>
2022-10-17 21:19:06 +0200

    feat: added properties to entities

--- api/src/reservations/entities/reservation.entity.ts
+++ api/src/reservations/entities/reservation.entity.ts
@@ -1,7 +1,46 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+import { ObjectId } from 'mongodb';
+import { Column, CreateDateColumn, ObjectIdColumn, UpdateDateColumn } from 'typeorm';
 
-@ObjectType()
+@ObjectType({ description: 'reservation' })
 export class Reservation {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => ID, { description: 'id of the reservation' })
+  @ObjectIdColumn()
+  id: ObjectId;
+  
+  @Field()
+  @Column()
+  userId: string;
+
+  @Field({ defaultValue: 0 })
+  @Column()
+  rooms: number;
+
+  @Field()
+  @Column()
+  amountAdults: number;
+
+  @Field()
+  @Column()
+  amountChildren: number;
+
+  @Field()
+  @Column()
+  price: number;
+
+  @Field()
+  @Column()
+  reservationStartDate: Date;
+
+  @Field()
+  @Column()
+  reservationEndDate: Date;
+
+  @Field({ nullable: true })
+  @CreateDateColumn({ type: 'timestamp', nullable: true })
+  createdAt?: Date;
+
+  @Field({ nullable: true })
+  @UpdateDateColumn({ type: 'timestamp', nullable: true })
+  updatedAt?: Date;
 }
--- api/src/rooms/entities/room.entity.ts
+++ api/src/rooms/entities/room.entity.ts
@@ -1,7 +1,64 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ObjectIdColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { ObjectId } from 'mongodb';
+import { Reservation } from "src/reservations/entities/reservation.entity";
 
-@ObjectType()
+@Entity()
+@ObjectType({ description: 'room' })
 export class Room {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => ID, { description: 'id of the room' })
+  @ObjectIdColumn({ name: '_id' })
+  id: ObjectId;
+
+  @Field() // GraphQL
+  @Column() //typeORM
+  name: string;
+
+  @Field()
+  @Column()
+  description: string;
+
+  @Field(() => Reservation)
+  reservation: Reservation;
+
+  @Column()
+  reservationId: string;
+
+  @Field()
+  @Column()
+  rating: number;
+
+  @Field()
+  @Column()
+  category: string;
+
+  @Field()
+  @Column()
+  roomsAvailable: number;
+
+  @Field()
+  @Column()
+  cleaned?: boolean;
+
+  @Field()
+  @Column()
+  occupied: boolean;
+
+  @Field()
+  @Column()
+  roomAccesCode: string;
+
+  @Field({ nullable: true })
+  @CreateDateColumn({ type: 'timestamp', nullable: true })
+  createdAt?: Date;
+
+  @Field({ nullable: true })
+  @UpdateDateColumn({ type: 'timestamp', nullable: true })
+  updatedAt?: Date;
 }
