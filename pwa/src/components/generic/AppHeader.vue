27d0e8bcd6cb679fcee2f1b623493a9651a430a4
Carmino <carmino.deschuijmere@student.howest.be>
2022-10-17 22:52:46 +0200

    fix: changed padding for small screen navbar

--- pwa/src/components/generic/AppHeader.vue
+++ pwa/src/components/generic/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="px-6 mb-6 py-6 md:py-0 md:bg-themeOffWhite drop-shadow-md">
+    <header class="px-6 py-6 md:py-0 md:bg-themeOffWhite drop-shadow-md">
         <nav class="mx-auto flex max-w-6xl items-center justify-between">
             <router-link class="rounded-md outline-none focus-visible:right-2" to="/">
                 <Building />
