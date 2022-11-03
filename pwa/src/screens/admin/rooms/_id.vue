b0f8fa7076fdee7668a7804aefc356ea943120be
Klaudia <klaudia.warmus@student.howest.be>
2022-11-03 10:14:56 +0100

    feat: pick room data

--- pwa/src/screens/admin/rooms/_id.vue
+++ pwa/src/screens/admin/rooms/_id.vue
@@ -3,40 +3,23 @@
     <section class="flex h-full w-full">
       <admin-navigation />
       <div class="w-5/6 p-6">
-        <admin-header :name="`Room ${params.id}`" />
+        <admin-header :name="`Room ${result?.room.name}`" />
+        <h2 class="font-title pl-6 font-bold">{{ result?.room.id }}</h2>
         <div class="p-6 lg:flex lg:justify-evenly lg:gap-6">
-          <div>
-            <div>
-              <label
-                for="roomAvailable"
-                class="flex cursor-pointer items-center"
-              >
-                <div class="relative">
-                  <input
-                    id="roomAvailable"
-                    type="checkbox"
-                    class="peer sr-only"
-                  />
-                  <div
-                    class="peer-checked:bg-themeBrown h-4 w-10 rounded-full bg-gray-400 shadow-inner"
-                  ></div>
-                  <div
-                    class="absolute -left-1 -top-1 h-6 w-6 rounded-full bg-white shadow transition peer-checked:translate-x-full"
-                  ></div>
-                </div>
-                <div class="ml-3">Available</div>
-              </label>
-            </div>
+          <div v-if="loading">
+            <p>Loading</p>
+          </div>
+          <div v-else-if="error">
+            <p>Error happened.</p>
+          </div>
+          <div v-else-if="result">
             <img
               class="lg:w-94 mb-6 mt-6 aspect-video rounded-md object-cover shadow-md"
               :src="luxe"
               :alt="`picture of a -suite`"
             />
             <p class="leading-8 lg:max-w-sm">
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-              erat, sed diam voluptua. At vero eos et accusam et justo duo
-              dolores.
+              {{ result.room.description }}
             </p>
             <p class="py-6">Price per night: <span>120$</span></p>
           </div>
