4f6bea07d83d2059a3e341f35d6de2f21aef2afa
Carmino <carmino.deschuijmere@student.howest.be>
2022-12-15 18:28:27 +0100

    feat: finished id pages

--- pwa/src/screens/rooms/_id.vue
+++ pwa/src/screens/rooms/_id.vue
@@ -8,23 +8,27 @@
         </div>
         <div v-else-if="result" class="grid grid-rows-[auto_1fr] md:grid-cols-2 md:gap-12 gap-6">
             <img v-if="result.room.category.toLowerCase() == 'luxe'" class="w-full object-cover aspect-video rounded-md"
-                :src="luxe" :alt="`picture of a ${result.room.category.toLowerCase() }-suite`" />
-            <img v-else-if="result.room.category.toLowerCase() == 'standard'" class="w-full object-cover aspect-video rounded-md"
-                :src="standard" :alt="`picture of a ${result.room.category}-suite`" />
+                :src="luxe" :alt="`picture of a ${result.room.category.toLowerCase()}-suite`" />
+            <img v-else-if="result.room.category.toLowerCase() == 'standard'"
+                class="w-full object-cover aspect-video rounded-md" :src="standard"
+                :alt="`picture of a ${result.room.category}-suite`" />
             <img v-else class="w-full object-cover aspect-video rounded-md" :src="standard"
                 :alt="`picture of a standard-suite`" />
-            <div>
-                <ul class="flex gap-1 mb-3">
-                    <li v-for="n in result.room.rating">
-                        <Star class="fill-themeBrown stroke-themeBrown" />
-                    </li>
-                    <li v-if="result.room.rating < 5" v-for="n in 5 - result.room.rating">
-                        <Star />
-                    </li>
-                </ul>
-                <p class="tracking-wide md:text-lg mb-3">
-                    {{ result.room.description }}
-                </p>
+            <div class=" flex flex-col justify-between">
+                <div>
+                    <ul class="flex gap-1 mb-3">
+                        <li v-for="n in result.room.rating">
+                            <Star class="fill-themeBrown stroke-themeBrown" />
+                        </li>
+                        <li v-if="result.room.rating < 5" v-for="n in 5 - result.room.rating">
+                            <Star />
+                        </li>
+                    </ul>
+                    <p class="tracking-wide md:text-lg mb-3">
+                        {{ result.room.description }}
+                    </p>
+                </div>
+
                 <p class="font-semibold">Price: € {{ result.room.category.toLowerCase() === 'luxe' ? 500 : 300 }}</p>
             </div>
 
