d8ba059b6157212fb91b9766fe2cfded0cf0cff2
Carmino <carmino.deschuijmree@student.howest.be>
2022-12-17 10:29:51 +0100

    Revert "Revert "Merge remote-tracking branch 'origin/feature/docker' into feature/docker""
    This reverts commit 4e6d444f9acd37a01ce98ee5867314c964fdf199.

--- api/src/bootstrap/bootstrap.module.ts
+++ api/src/bootstrap/bootstrap.module.ts
@@ -1,8 +1,8 @@
-import { DataSource } from 'typeorm';
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { DataSource } from 'typeorm'
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 
 @Module({
   imports: [
@@ -14,7 +14,8 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         type: 'mongodb',
-        url: 'mongodb://localhost:27037/api',
+        url: 'mongodb://localhost:27017/api', // Local
+        //url: 'mongodb://db:localhost:27037/api', //docker
         entities: [__dirname + '/../**/*.entity.{js,ts}'],
         synchronize: true, // Careful with this in production
         useNewUrlParser: true,
@@ -22,8 +23,8 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
       }),
 
       dataSourceFactory: async (options) => {
-        const dataSource = await new DataSource(options).initialize();
-        return dataSource;
+        const dataSource = await new DataSource(options).initialize()
+        return dataSource
       },
     }),
   ],
